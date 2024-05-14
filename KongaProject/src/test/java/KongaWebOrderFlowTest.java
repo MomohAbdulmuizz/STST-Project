f938ba9e5a6e78b51b30814776f46c429db9418d
MomohAbdulmuizz <momohabdulmuizz@gmail.com>
2024-05-14 15:08:06 +0100

    Started my Konga Project

---
+++ KongaProject/src/test/java/KongaWebOrderFlowTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+
+/**
+ 1. Visit the URL Konga
+ 2. Sign in to Konga Successfully
+ 3. From the Categories, click on the “Computers and Accessories”
+ 4. Click on the Laptop SubCategory
+ 5. Click on the Apple MacBooks
+ 6. Add an item to the cart
+ 7. Select Address
+ 8. Continue to make payment
+ 9. Select a Card Payment Method
+ 10. Input invalid card details
+ 11. Print Out the error message: Invalid card number
+ 12. Close the iFrame that displays the input card Modal
+ 13. Quit the browser
+ */
+
+public class KongaWebOrderFlowTest {
+    //Import Selenium WebDriver
+    private WebDriver driver;
+
+
+
+
+
+
+}
